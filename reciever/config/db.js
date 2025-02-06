3dc20b64f548cd647c1111ac4d630afd3c8401e9
hritikthakur-kiwi <hritik.thakur@kiwitech.com>
2025-02-06 11:45:22 +0530

    updated

--- reciever/config/db.js
+++ reciever/config/db.js
@@ -8,8 +8,8 @@ export const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'db', // Ensure it uses 'db' inside Docker
-    port: process.env.DB_PORT || 5432, // Ensure port is set
+    host: process.env.DB_HOST || 'db',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
   }
